456b36d25118198c78c0f721465cbfa7e7601774
Rosesharon21 <shuttsell@ivytech.edu>
2024-12-13 02:21:28 -0500

    Add files via upload

---
+++ Module 8 Assignment - functions in Python.py
@@ -0,0 +1,12 @@
+def greaterThan(x, y):
+    return x > y
+
+# Assign values to variables
+a = 10
+b = 6
+
+# Call the function and store the result
+result = greaterThan(a, b)
+
+# Print the output
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result) + ".")
